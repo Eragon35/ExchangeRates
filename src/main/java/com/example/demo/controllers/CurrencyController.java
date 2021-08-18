337e7fa48634f55f325559ab338e48f06a0dd2d2
Eragon35 <antipin35@yahoo.com>
2021-08-18 21:29:51 +0300

    Another one not working controller

--- src/main/java/com/example/demo/controllers/CurrencyController.java
+++ src/main/java/com/example/demo/controllers/CurrencyController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class CurrencyController {
+    @PostMapping("ratio")
+    public double ratio(@RequestBody String cur1, String cur2) {
+        System.out.println(cur1 + " " + cur2);
+        double ratio = 2.0;
+        return ratio;
+    }
 }
