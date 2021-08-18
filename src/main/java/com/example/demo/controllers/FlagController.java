d009ed1a30f2f1ea4ba9acbcf1908ada372f6316
Eragon35 <antipin35@yahoo.com>
2021-08-18 21:16:07 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/FlagController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class FlagController {
+
+    
+}
