38e9e4d23c5688f7a0d35d639d6ab08df61ad8ae
Eragon35 <antipin35@yahoo.com>
2021-08-18 21:20:54 +0300

    Commit with not working flag controller

--- src/main/java/com/example/demo/controllers/FlagController.java
+++ src/main/java/com/example/demo/controllers/FlagController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +9,19 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class FlagController {
 
-    
+    @GetMapping("/flags")
+    public @ResponseBody List<BufferedImage> getFlags() throws IOException {
+        ArrayList<BufferedImage> pack = new ArrayList<>();
+        for (final File file : Objects.requireNonNull(new ClassPathResource("/flags").getFile().listFiles())) {
+            pack.add(ImageIO.read(file.getAbsoluteFile()));
+        }
+        return pack;
+    }
 }
